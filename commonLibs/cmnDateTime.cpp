98400198e29ba90dbbb522bf64f7a9adc5feec48
gyuho.jeong <jgh0721@gmail.com>
2024-06-18 16:59:28 +0900

    추가 : GetTimeOfDay 추가

--- commonLibs/cmnDateTime.cpp
+++ commonLibs/cmnDateTime.cpp
@@ -1,6 +1,14 @@
 ﻿#include "stdafx.h"
 #include "cmnDateTime.hpp"
 
+// epoch time으로 변환할 상수, oalGetTimeOfDay 에서 사용
+// microseconds betweeen Jan 1,1601 and Jan 1,1970 
+#if defined(_MSC_VER) || defined(_MSC_EXTENSIONS)
+    #define DELTA_EPOCH_IN_MICROSECS  11644473600000000Ui64
+#else
+    #define DELTA_EPOCH_IN_MICROSECS  11644473600000000ULL
+#endif
+
 namespace nsCmn
 {
     QDateTime ConvertTo( const FILETIME& Time, bool IsUTC, bool* ok /* = nullptr */ )
@@ -20,5 +28,64 @@ namespace nsCmn
             *ok = true;
 
         return tempDateTime;
+    }
+
+    void GetTimeOfDay( OAL_TIMEVAL* tv, OAL_TIMEZONE* tz )
+    {
+        // http://alones.kr/gettimeofday%EB%A5%BC-window%EC%97%90%EC%84%9C-%EA%B5%AC%ED%98%84%ED%95%B4%EC%84%9C-%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0/
+        // https://msdn.microsoft.com/en-us/library/windows/desktop/dn553408%28v=vs.85%29.aspx?f=255&MSPPError=-2147217396
+        FILETIME ft; /* 64-bit value representing the number of 100-nanosecond intervals since January 1, 1601 00:00 UTC */
+        unsigned __int64 tmpres = 0;
+        static int tzflag;
+
+        if( tv != nullptr )
+        {
+            // system time을 구하기
+            //if( QOperatingSystemVersion::current() >= QOperatingSystemVersion::Windows8 )
+            //    GetSystemTimePreciseAsFileTime( &ft );
+            //else
+                GetSystemTimeAsFileTime( &ft );
+
+            // unsigned 64 bit로 만들기
+            tmpres |= ft.dwHighDateTime;
+            tmpres <<= 32;
+            tmpres |= ft.dwLowDateTime;
+
+            // 100nano를 1micro로 변환하기
+            tmpres /= 10;
+
+            // epoch time으로 변환하기
+            tmpres -= DELTA_EPOCH_IN_MICROSECS;
+
+            // sec와 micorsec으로 맞추기
+            tv->tv_sec  = static_cast< long >( ( tmpres / 1000000UL ) );
+            tv->tv_usec = ( tmpres % 1000000UL );
+        }
+
+        // timezone 처리
+        if( tz != nullptr )
+        {
+            DWORD Ret = 0;
+            TIME_ZONE_INFORMATION TZ;
+            if( (Ret = GetTimeZoneInformation( &TZ )) != TIME_ZONE_ID_INVALID )
+            {
+                if( Ret == TIME_ZONE_ID_UNKNOWN )
+                {
+                    tz->tz_minuteswest = 0;
+                    tz->tz_dsttime = 0;
+                }
+                else
+                {
+                    tz->tz_minuteswest = TZ.Bias;
+                    tz->tz_dsttime = 0;
+
+                    if( Ret == TIME_ZONE_ID_STANDARD )
+                        tz->tz_dsttime = TZ.StandardBias;
+
+                    if( Ret == TIME_ZONE_ID_DAYLIGHT )
+                        tz->tz_dsttime = TZ.DaylightBias;
+                }
+            }
+        }
     }
 } // nsCmn
--- commonLibs/cmnDateTime.hpp
+++ commonLibs/cmnDateTime.hpp
@@ -2,7 +2,34 @@
 
 namespace nsCmn
 {
+    //////////////////////////////////////////////////////////////////////////
+    /// Time
+
+    struct OAL_TIMEVAL
+    {
+        long tv_sec;        // time_t 와 같음
+        long tv_usec;       // 초 이하의 microsecond 
+    };
+
+    struct OAL_TIMEZONE
+    {
+        int  tz_minuteswest; /* minutes W of Greenwich */
+        int  tz_dsttime;     /* type of dst correction */
+    };
+
+    struct OAL_SYSTEMTIME
+    {
+        WORD wYear;
+        WORD wMonth;
+        WORD wDayOfWeek;
+        WORD wDay;
+        WORD wHour;
+        WORD wMinute;
+        WORD wSecond;
+        WORD wMilliseconds;
+    };
+
     QDateTime                           ConvertTo( const FILETIME& Time, bool IsUTC, bool* ok = nullptr );
 
-
+    void                                GetTimeOfDay( OAL_TIMEVAL* tv, OAL_TIMEZONE* tz );
 } // nsCmn
